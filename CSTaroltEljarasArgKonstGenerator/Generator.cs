b7b6422e3e986894298cc51cf8cd2428a1bf5380
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-04 10:33:02 +0100

    Add project files.

---
+++ CSTaroltEljarasArgKonstGenerator/Generator.cs
@@ -0,0 +1,58 @@
+﻿using d7p4n4Namespace.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSTaroltEljarasArgKonstGenerator
+{
+    class Generator
+    {
+        public static void generateClass(string outputPath, TaroltEljaras taroltEljaras)
+        {
+            string[] text = readIn("TemplateTaroltEljaras");
+
+            string replaced = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Contains("argumentumKonstans"))
+                {
+                    string newline = "";
+                    foreach (var arg in taroltEljaras.ArgumentumLista)
+                    {
+                        if (!arg.BelsoNev.Equals("@RETURN_VALUE"))
+                        {
+                            newline = newline + text[i + 1].Replace("#@argNev#", arg.BelsoNev).Replace("#argNev#", arg.BelsoNev.Substring(1)) + "\n";
+                            Console.WriteLine(newline);
+                        }
+                    }
+                    replaced = replaced + newline + "\n";
+                    i = i + 2;
+                }
+                replaced = replaced + text[i] + "\n";
+            }
+
+            replaced = replaced.Replace("#taroltEljarasNev#", taroltEljaras.Megnevezes);
+
+            writeOut(replaced, taroltEljaras.Megnevezes, outputPath);
+
+        }
+
+        public static string[] readIn(string fileName)
+        {
+
+            string textFile = "c:\\Templates\\c#\\Tabla\\" + fileName + ".csT";
+
+            string[] text = File.ReadAllLines(textFile);
+
+            return text;
+        }
+
+        public static void writeOut(string text, string fileName, string outputPath)
+        {
+            File.WriteAllText(outputPath + fileName + ".cs", text);
+
+        }
+    }
+}
---
+++ CSTaroltEljarasArgKonstGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using d7p4n4Namespace.Final.Class;
+using System;
+using System.IO;
+
+namespace CSTaroltEljarasArgKonstGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deserializer deserializer = new Deserializer();
+
+            try
+            {
+                string[] files =
+                    Directory.GetFiles("d:\\Server\\Visual_studio\\output_Xmls\\StorProcs\\", "*.xml", SearchOption.TopDirectoryOnly);
+
+                Console.WriteLine(files.Length);
+
+                foreach (var _file in files)
+                {
+                    TaroltEljaras ujTaroltEljaras = new TaroltEljaras();
+                    string _filename = Path.GetFileNameWithoutExtension(_file);
+                    Console.WriteLine(_filename);
+
+                    string textFile = Path.Combine("d:\\Server\\Visual_studio\\output_Xmls\\StorProcs\\", _filename + ".xml");
+
+                    string text = File.ReadAllText(textFile);
+
+                    ujTaroltEljaras = deserializer.deser(text);
+                    Generator.generateClass("d:\\Server\\Visual_studio\\AK\\outputTaroltEljaras\\", ujTaroltEljaras);
+
+                }
+            }
+            catch (Exception _exception)
+            {
+                Console.WriteLine(_exception.Message);
+            }
+        }
+    }
+}
